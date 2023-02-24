3aa55065621fa78c33b56aab81653b11069cf98c
Adelkilde <William.adelkilde@gmail.com>
2023-02-24 17:53:58 +0100

    Second commit
    Update to CSS - Begin JS soon

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict"
+window.addEventListener("load", start)
+
+let points = 0;
+let lives = 0;
+
+function start() {
+    console.log("JavaScript Runs!")
+
+    points = 0;
+    lives = 3;
+
+    document.querySelector("#gamergunk_container").classList.add("falling");
+    document.querySelector("#stinkypants_container").classList.add("falling");
+    document.querySelector("#hairyballs_container").classList.add("falling");
+    document.querySelector("#chadsoap_container").classList.add("falling");
+}
