f7e1cf095b136dc310271d9009644fe23bf03680
Adelkilde <William.adelkilde@gmail.com>
2023-02-28 22:01:52 +0100

    Random commit Tuesday :))
    Wtffff am I doinggggg

--- script.js
+++ script.js
@@ -10,11 +10,58 @@ function start() {
     points = 0;
     lives = 3;
 
-    document.querySelector("#gamergunk_container").classList.add("falling");
-    document.querySelector("#stinkypants_container").classList.add("falling");
-    document.querySelector("#hairyballs_container").classList.add("falling");
-    document.querySelector("#chadsoap_container").classList.add("falling");
-
-    document.querySelector("#gamergunk_container").addEventListener("click", clickCoin);
-  document.querySelector("#chadsoap_container").addEventListener("click", clickBomb);
+    startAnimations();
+    startPositions();
+    registerClick();
+    restartPositions();
 }
+
+function startAnimations(){
+  document.querySelector("#gamergunk1_container").classList.add("falling");
+  document.querySelector("#gamergunk2_container").classList.add("falling");
+  document.querySelector("#gamergunk3_container").classList.add("falling");
+  document.querySelector("#stinkypants_container").classList.add("falling");
+  document.querySelector("#chadsoap_container").classList.add("falling");
+  document.querySelector("#hairyballs_container").classList.add("falling");
+}
+function startPositions(){}
+function registerClick(){}
+function restartPositions(){}
+
+function gunkClicked() {
+  console.log("Click gunk");
+  const gunk = this;
+
+  gunk.removeEventListener("click", clickGunk);
+}
+
+function gunkGone() {
+
+}
+
+function gunkClicked() {}
+
+function gunkGone() {}
+
+function gunkRestart() {}
+
+function underwearClicked() {}
+
+function underwearGone() {}
+
+function soapClicked() {}
+
+function soapGone() {}
+
+function ballsClicked() {}
+
+function ballsGone() {}
+
+function incrementPoints() {}
+
+function displayPoints() {
+  console.log("show points")
+  document.querySelector("#gunk_count").textContent = points;
+}
+
+function decrementLives() {}
